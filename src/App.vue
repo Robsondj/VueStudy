b81fa72de00d5ddc0e4d2bbcd27981588af55bb1
Robson de Jesus <robson.djsilva@gmail.com>
2021-11-01 11:11:45 -0300

    Added v-for example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
-    <img :alt="photo.title" :src="photo.url">
+    <ul>
+      <li v-for="(photo, index) of photos" :key="index">
+        <img :alt="photo.title" :src="photo.url">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,11 +14,17 @@ export default {
   
   data () {
     return {
-      title: 'Welcome to Your Vue.js App',
-      photo: {
-        url: "./src/assets/logo.png",
-        title: "Vue photo"
-      }
+      title: 'Welcome Vue.js App',
+      photos: [
+        {
+          url: "./src/assets/logo.png",
+          title: "Vue photo"
+        },
+        {
+          url: "https://f.hubspotusercontent30.net/hubfs/8797985/Imported_Blog_Media/17_05_-2.png",
+          title: "Vue Web photo"
+        }
+      ]
     }
   }
 }
