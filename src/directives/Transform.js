ad3c487abf1cb45c66c281ddfdb9ee38808eadd1
Robson de Jesus <robson.djsilva@gmail.com>
2021-11-02 11:16:03 -0300

    Added directive example to the project

---
+++ src/directives/Transform.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+
+Vue.directive('transform', {
+
+    bind(el, binding, vnode) {
+
+        let current = 0;
+        el.addEventListener('dblclick', function() {
+
+            let increment = 90;
+            let animate = false;
+
+            if(binding.value) {
+                increment = binding.value.increment;
+                animate = binding.value.animate;
+            }
+            current += increment;
+            this.style.transform = `rotate(${current}deg)`;
+            if(animate) {
+                this.style.transition = "transform 0.5s";
+            }
+        });
+    }
+
+});
