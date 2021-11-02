0e8b1af18f771524b40ad7a33d743a55e8a4549f
Robson de Jesus <robson.djsilva@gmail.com>
2021-11-02 16:00:48 -0300

    Added argument example. The argument is the reverse and the modifier is the animate

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -8,9 +8,9 @@ Vue.directive('transform', {
         el.addEventListener('dblclick', function() {
 
             let increment = binding.value || 90;
-            let animate = false;
 
-            current += increment;
+            current = (binding.arg == 'reverse'? current-increment : current+increment) ;
+            
             this.style.transform = `rotate(${current}deg)`;
             if(binding.modifiers.animate) {
                 this.style.transition = "transform 0.5s";
