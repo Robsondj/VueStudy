56c4857376dbc790fee3ee9c52e7975dfa1e0c72
Robson de Jesus <robson.djsilva@gmail.com>
2021-11-03 23:53:44 -0300

    Example of data association with v-on and v-bind to update html field and object

--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -1,23 +1,103 @@
 <template>
-    <div>
-        <h1 class="center">{{ title }}</h1>
-    </div>
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+
+    <form @submit.prevent="save()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off"
+               @input="photo.title = $event.target.value"
+               :value="photo.title"
+        >
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off"
+               @input="photo.url = $event.target.value"
+               :value="photo.url"
+        >
+        <responsive-img/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off"
+                  @input="photo.description = $event.target.value"
+                  :value="photo.description"
+        ></textarea>
+      </div>
+
+      <div class="centralizado">
+        <form-button label="GRAVAR" type="submit"/>
+        <router-link to="/"><form-button label="VOLTAR" type="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
 
 <script>
 
-export default {
-    
-    data() {
-        return {
-            title: "Cadastro"
-        }
-    }
-}
-</script>
+import ResponsiveImg from '../shared/responsive-img/ResponsiveImg.vue'
+import Button from '../shared/button/Button.vue';
 
-<style scoped>
-  .center {
-      text-align: center;
+export default {
+
+  components: {
+
+    'responsive-img': ResponsiveImg, 
+    'form-button': Button
+  },
+
+  data() {
+      return {
+          photo: {
+              title: '',
+              url: '',
+              description: ''
+          }
+      }
+  },
+
+  methods: {
+      save() {
+          console.log(this.photo);
+          this.photo = {
+              title: '',
+              url: '',
+              description: ''
+          }
+      }
   }
+}
+
+</script>
+<style scoped>
+
+  .centralizado {
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
 </style>
